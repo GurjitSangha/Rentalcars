325f9a3e2cd5db86bb29fb44a6307c31c7fe9944
Gurjit Sangha <gsangha1@sheffield.ac.uk>
2016-02-11 10:29:57 +0000

    Vehicles specs now show

--- src/com/rentalcars/Main.java
+++ src/com/rentalcars/Main.java
@@ -5,15 +5,19 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 
 public class Main {
 
     private static ArrayList<Vehicle> vehicles = new ArrayList<>();
+    private static HashMap<String, String> carMap = new HashMap<>();
+    private static HashMap<String, String> doorsCarMap = new HashMap<>();
+    private static HashMap<String, String> transmissionMap = new HashMap<>();
+    private static HashMap<String, String> fuelAirConMap = new HashMap<>();
 
     public static void main(String[] args) {
         // Exit if json file path not specified
@@ -22,9 +26,12 @@ public class Main {
             System.exit(0);
         }
 
+        // Setup the hash maps from the table in the document
+        setupHashMaps();
+
         parseJsonFile(args[0]);
         sortByPrice();
-
+        specBySIPP();
     }
 
     private static void parseJsonFile(String jsonFile) {
@@ -70,4 +77,74 @@ public class Main {
             System.out.printf("%2d. %s - %s\n", i+1, vehicles.get(i).getName(), vehicles.get(i).getPrice());
         }
     }
+
+    private static void specBySIPP() {
+        // Get the spec for each vehicle in the list
+        System.out.println("Vehicle specifications");
+        for(int i = 0; i < vehicles.size(); i++) {
+            Vehicle vehicle = vehicles.get(i);
+            getSpecForVehicle(i + 1, vehicle.getName(), vehicle.getSipp());
+        }
+    }
+
+    private static void setupHashMaps() {
+        // Car type map
+        carMap.put("M", "Mini");
+        carMap.put("E", "Economy");
+        carMap.put("C", "Compact");
+        carMap.put("I", "Intermediate");
+        carMap.put("S", "Standard");
+        carMap.put("F", "Full size");
+        carMap.put("P", "Premium");
+        carMap.put("L", "Luxury");
+        carMap.put("X", "Special");
+
+        // Doors/car type map
+        doorsCarMap.put("B", "2 doors");
+        doorsCarMap.put("C", "4 doors");
+        doorsCarMap.put("D", "5 doors");
+        doorsCarMap.put("W", "Estate");
+        doorsCarMap.put("T", "Convertible");
+        doorsCarMap.put("F", "SUV");
+        doorsCarMap.put("P", "Pick up");
+        doorsCarMap.put("V", "Passenger Van");
+
+        // Transmission map
+        transmissionMap.put("M", "Manual");
+        transmissionMap.put("A", "Automatic");
+
+        // Fuel/Air con map
+        fuelAirConMap.put("N", "Petrol/no AC");
+        fuelAirConMap.put("R", "Petrol/AC");
+    }
+
+    private static void getSpecForVehicle(int index, String name, String sipp) {
+        // Get the letters from the sipp
+        String carKey = sipp.substring(0,1);
+        String doorsCarKey = sipp.substring(1,2);
+        String transmissionKey = sipp.substring(2,3);
+        String fuelKey = sipp.substring(3,4);
+
+        // Find the values for each key
+        String carType = carMap.get(carKey);
+        String doorsCarType = doorsCarMap.get(doorsCarKey);
+        String transmissionType = transmissionMap.get(transmissionKey);
+        String fuelAirConType = fuelAirConMap.get(fuelKey);
+
+        // Split the fuel air con into two strings
+        String[] fuelACArray = fuelAirConType.split("/");
+
+        // Replace any null values with n/a
+        carType = (carType == null) ? "n/a" : carType;
+        doorsCarType = (doorsCarType == null) ? "n/a" : doorsCarType;
+        transmissionType = (transmissionType == null) ? "n/a" : transmissionType;
+        fuelACArray[0] = (fuelACArray[0] == null) ? "n/a" : fuelACArray[0];
+        fuelACArray[1] = (fuelACArray[1] == null) ? "n/a" : fuelACArray[1];
+
+        // Print the result
+        System.out.printf("%2d. %s - %s - %s - %s - %s - %s - %s\n",
+                index, name, sipp, carType, doorsCarType, transmissionType, fuelACArray[0], fuelACArray[1]);
+    }
+
+
 }
